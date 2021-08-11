76d75297d5e66c886d780fe9c49489f88c67b66c
Nathan <natamcculloch@gmail.com>
2021-08-11 12:18:42 +1200

    Updated Flask to read config from config file

--- leaderboard/__init__.py
+++ leaderboard/__init__.py
@@ -9,7 +9,11 @@ from werkzeug.wrappers import response
 
 app = Flask(__name__, instance_relative_config = True)
 CORS(app, support_credentials = True)
-app.config["DEBUG"] = True
+
+
+# Load the file specified by the APP_CONFIG_FILE environment variable
+# Variables defined here will override those in the default configuration
+app.config.from_envvar('APP_CONFIG_FILE')
 
 
 # PostgreSQL database connection parameters
